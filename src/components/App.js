1df7d21f8138f5fdde78588c56a841c770440e07
nicmackenzie <nicmackenzie@live.com>
2023-05-25 08:07:48 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [imageUrl, setImageUrl] = useState('');
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        setImageUrl(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  return (
+    <>
+      {!isLoaded && <p>Loading...</p>}
+      {isLoaded && <img src={imageUrl} alt="A Random Dog" />}
+    </>
+  );
+}
+
+export default App;
